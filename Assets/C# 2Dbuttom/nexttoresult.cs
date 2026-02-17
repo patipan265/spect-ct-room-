dfc242e3050c8f8f3d12e4bb1e4c4950c131893e
BeltKawin <15noelleb@gmail.com>
2026-02-17 20:43:08 +0700

    ส่งๆๆๆ

---
+++ Assets/C# 2Dbuttom/nexttoresult.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nexttoresult : MonoBehaviour
+{
+
+    public void nextresult()
+    {
+        SceneManager.LoadScene("Result_image");
+    }
+
+
+}
---
+++ Assets/C# 2Dbuttom/retostart.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class retostart: MonoBehaviour
+{
+
+    public void nextstart()
+    {
+    SceneManager.LoadScene("Mainmenu");
+}
+
+
+}
