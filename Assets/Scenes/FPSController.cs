6ac60d0df8bc7c989193e1ff48da1ca11c228884
patipan265 <0860941343m@gmail.com>
2026-02-22 22:51:50 +0700

    6

---
+++ Assets/Scenes/FPSController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    public float speed = 3.5f;
+    public float mouseSensitivity = 200f;
+    public Transform playerCamera;
+
+    float xRotation = 0f;
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
